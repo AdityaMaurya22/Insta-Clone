f259ec665fc96f07f8e576596013dbc38ff735ee
AdityaMaurya22 <mauryaaditya2203@gmail.com>
2026-03-16 23:41:49 +0530

    deployment error fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'https://insta-clone-5kpf.onrender.com'
 }))
 
 app.use('/api/auth', authRouter)
